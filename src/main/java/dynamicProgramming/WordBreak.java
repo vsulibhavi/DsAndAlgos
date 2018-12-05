31945d343cad9551c867d6b74344ab0cb7a3325b
vishal <vishal@fptechscience.com>
2018-12-05 14:26:32 +0530

    word break problem

---
+++ src/main/java/dynamicProgramming/WordBreak.java
@@ -0,0 +1,70 @@
+package dynamicProgramming;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class WordBreak {
+
+    public static Set<String> dict = new HashSet<>(Arrays.asList(new String[]{"i", "like", "sam", "sung", "samsungs", "mobile", "ice",
+            "cream", "icecream", "man", "go", "mango"}));
+
+    public static void main(String[] args){
+
+        String word = "ilikeSamsungsmangoicecream";
+
+     //   wordBreak("ilikeSamsung");
+        System.out.println(wordBreakSpellCorrect(word.toLowerCase().toCharArray(),0,word.length()-1));
+        int[] lookup = new int[word.length() + 1];
+        Arrays.fill(lookup, -1);
+
+        printAllWordBreak(word.toLowerCase().toCharArray(),0,word.length()-1,"");
+
+    }
+
+    private static boolean wordBreakSpellCorrect(char[] spellWord,int start,int end) {
+
+        if(start >end ) return true;
+        StringBuffer stringBuffer = new StringBuffer();
+
+        for(int i  =start;i<=end;i++){
+
+            stringBuffer.append(spellWord[i]);
+            if(dict.contains(stringBuffer.toString()))
+            {
+                System.out.println("start "+start +" end" + end);
+               boolean otherWordCorrect =  wordBreakSpellCorrect(spellWord,i+1,end);
+               if(otherWordCorrect == false)
+                   continue;
+               return true;
+            }
+
+
+        }
+        return false;
+
+    }
+
+
+    public static void printAllWordBreak(char[] spellWord,int start,int end,String correctedWord){
+
+
+        if(start >end )
+            System.out.println(correctedWord);
+        StringBuffer stringBuffer = new StringBuffer();
+
+        for(int i  =start;i<=end;i++){
+
+            stringBuffer.append(spellWord[i]);
+            if(dict.contains(stringBuffer.toString()))
+            {
+                printAllWordBreak(spellWord,i+1,end,correctedWord + " "+ stringBuffer.toString());
+
+            }
+
+        }
+
+
+    }
+
+
+}
